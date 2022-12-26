07070353475aefb5414fdb9137dc45d4d7c1fa9f
atharv_arolkar <atharv_arolkar@persistent.com>
2022-12-26 11:07:13 +0530

    push

--- pages/_app.js
+++ pages/_app.js
@@ -4,6 +4,14 @@ import '../styles/globals.css'
 export default function App({ Component, pageProps }) {
   return (
     <ProductsContextProvider>
+      {useEffect(() => {
+    window.OneSignal = window.OneSignal || [];
+    OneSignal.push(function () {
+      OneSignal.init({
+        appId: "1dc2c7d5-bf47-4500-8bfe-d5eed854a86e",
+      });
+    });
+  }, [])}
 <Component {...pageProps} />
     </ProductsContextProvider>
   )
