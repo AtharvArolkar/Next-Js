2c8c607aaa3d4f7d671487e36a104c0d3e79102d
atharv_arolkar <atharv_arolkar@persistent.com>
2022-12-26 14:53:12 +0530

    .

--- pages/index.js
+++ pages/index.js
@@ -76,7 +76,7 @@ export default function Home({products}) {
         <script src="/Push-Notification-OneSignal-main/public/js/index.js"></script>
       </Head> */}
 
-      {/* <main className={styles.main}>
+      <main className={styles.main}>
         <div className={styles.grid}>
           <a href="https://nextjs.org/docs" className={styles.card}>
             <h2>Documentation &rarr;</h2>
@@ -109,7 +109,7 @@ export default function Home({products}) {
           </a>
         </div>
       </main>
-*/}
+
       <footer className={styles.footer}>
         <a
           href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
