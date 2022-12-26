32ec61eb15ce8f934ea1f4ebf57a3016565e61d8
atharv_arolkar <atharv_arolkar@persistent.com>
2022-12-26 14:50:55 +0530

    .

--- pages/index.js
+++ pages/index.js
@@ -109,7 +109,7 @@ export default function Home({products}) {
           </a>
         </div>
       </main>
-
+*/}
       <footer className={styles.footer}>
         <a
           href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
@@ -122,7 +122,7 @@ export default function Home({products}) {
           </span>
 
         </a>
-      </footer> */}
+      </footer> 
         
     </Layout>
   )
