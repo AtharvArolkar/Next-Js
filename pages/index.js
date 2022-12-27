7c31961f119681416c91ff51a2bd8a69ad578883
Tanad Bandodkar <56749596+TanadBandodkar@users.noreply.github.com>
2022-12-27 15:56:06 +0530

    Update index.js
    Update button

--- pages/index.js
+++ pages/index.js
@@ -6,6 +6,7 @@ import { useEffect, useState } from "react";
 import ProductAPI from "../components/ProductAPI";
 import Layout from "../components/Layout";
 import Head from "next/head";
+import Script from 'next/script'
 
 export default function Products({products}){
     const[phrase, setPhrase] =useState([]);
@@ -37,6 +38,9 @@ export default function Products({products}){
           
           </Head> */}
           <div>
+        <button className={`addbutton btn waves-effect waves-light`} >Add to home screen
+            {/* <i className="material-icons right">send</i> */}
+          </button>
           {categoryNames.map(catName => 
             <div key={catName}>
               {products.find(p => p.category === catName) && (
@@ -58,6 +62,47 @@ export default function Products({products}){
              
             </div>
           )}  
+          <Script>
+        {`
+            let deferredPrompt;
+            const addBtn = document.querySelector(".addbutton");
+            console.log(addBtn);
+            addBtn.style.display = "none";
+            
+            window.addEventListener("beforeinstallprompt", (e) => {
+                // Prevent Chrome 67 and earlier from automatically showing the prompt
+                e.preventDefault();
+                // Stash the event so it can be triggered later.
+                deferredPrompt = e;
+                // Update UI to notify the user they can add to home screen
+                addBtn.style.display = "block";
+                listenToUserAction();
+            });
+            
+            // listen to install button clic
+            function listenToUserAction() {
+                addBtn.addEventListener("click", presentAddToHome);
+            }
+            
+            // addBtn.addEventListener("click", (e) => {
+            
+            function presentAddToHome() {
+                // hide our user interface that shows our A2HS button
+                addBtn.style.display = "none";
+                // Show the prompt
+                deferredPrompt.prompt();
+                // Wait for the user to respond to the prompt
+                deferredPrompt.userChoice.then((choiceResult) => {
+                    if (choiceResult.outcome === "accepted") {
+                        console.log("User accepted the A2HS prompt");
+                    } else {
+                        console.log("User dismissed the A2HS prompt");
+                    }
+                    deferredPrompt = null;
+                });
+            };
+        `}
+      </Script>
         </div>
         </Layout>
     )
