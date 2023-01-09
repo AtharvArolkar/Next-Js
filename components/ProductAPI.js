f0444f474943b9b11503e13ac9296c36e32cc975
floating-dynamo <shridharmaskeri@gmail.com>
2023-01-09 20:29:38 +0530

    Added add-to-cart functionality, Added the simple custom service worker, Added list of charities

--- components/ProductAPI.js
+++ components/ProductAPI.js
@@ -1,34 +1,63 @@
-import { set } from 'mongoose'
-import Image from 'next/image'
-import { useContext } from 'react'
-import { ProductsContext } from './ProductsContext'
-export default function ProductAPI(props){
-    // console.log(picture[0])
-    // const pic=picture.split(",")
-   
-    // console.log({props})
-    const myLoader=({src})=>{
-      return props.product.images[0];
-    }
-    return (
-        <div className='w-64 p-1 border rounded-xl p-2'>
-              <div className='bg-white p-5 rounded-xl flex h-80 justify-center items-center'>
-                {/* {console.log(props.product.images[0])} */}
-                <Image className='rounded-xl' loader={myLoader} src={props.product.images[0]} alt='' width='240' height='240'/>
-              </div>
-              <div className='h-auto'></div>
-              <div className=' h-40 relative'>
-                <div className='mt-2'>
-                  <h3 className='font-bold text-lg'>{props.product.title}</h3>
-                </div>
-                <p className='text-sm mt-1 leading-5'>{props.product.description.slice(0,100)}...</p>
-                <div className='h-auto'></div>
-                <div className='flex absolute pt-2 inset-x-0 bottom-0'>
-                  <div className='flex-grow text-2xl font-bold'>${props.product.price}</div>
-                  <button onClick="" className='bg-black text-white py-1 px-3 rounded-xl'>Add To Cart</button>
-                </div>
-              </div>
-            </div>
-    )
-}
+import { set } from "mongoose";
+import Image from "next/image";
+import { ProductsContext } from "./ProductsContext";
+import React, { useContext, useEffect, useState } from "react";
+import CartContext from "../context/CartContext";
 
+export default function ProductAPI(props) {
+  // console.log(picture[0])
+  // const pic=picture.split(",")
+
+  // console.log({props})
+  const { cart, setCart } = useContext(CartContext);
+
+  const myLoader = ({ src }) => {
+    return props.product.images[0];
+  };
+
+  const addToCart = (product) => {
+    setCart((prevCart) => {
+      return [...prevCart, product];
+    });
+    alert(`${product.title} has been added to the cart!`);
+  };
+
+  return (
+    <div className="w-64 p-1 border rounded-xl p-2">
+      <div className="bg-white p-5 rounded-xl flex h-80 justify-center items-center">
+        {/* {console.log(props.product.images[0])} */}
+        <Image
+          className="rounded-xl"
+          loader={myLoader}
+          src={props.product.images[0]}
+          alt=""
+          width="240"
+          height="240"
+        />
+      </div>
+      <div className="h-auto"></div>
+      <div className=" h-40 relative">
+        <div className="mt-2">
+          <h3 className="font-bold text-lg">{props.product.title}</h3>
+        </div>
+        <p className="text-sm mt-1 leading-5">
+          {props.product.description.slice(0, 100)}...
+        </p>
+        <div className="h-auto"></div>
+        <div className="flex absolute pt-2 inset-x-0 bottom-0">
+          <div className="flex-grow text-2xl font-bold">
+            ${props.product.price}
+          </div>
+          <button
+            onClick={() => {
+              addToCart(props.product);
+            }}
+            className="bg-black text-white py-1 px-3 rounded-xl"
+          >
+            Add To Cart
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
--- pages/checkout.js
+++ pages/checkout.js
@@ -1,8 +1,55 @@
+import Image from "next/image";
+import { useContext } from "react";
 import Header from "../components/Header";
 import Layout from "../components/Layout";
+import CartContext from "../context/CartContext";
 
-export default function Checkout(){
-    return <Layout>
-        Checkout
+export default function Checkout() {
+  const { cart, setCart } = useContext(CartContext);
+
+  if (cart.length === 0) {
+    return (
+      <Layout>
+        <h1>The cart is currently Empty!</h1>
+      </Layout>
+    );
+  }
+
+  return (
+    <Layout>
+      {cart?.map((product) => {
+        return (
+          <div className="w-64 p-1 border rounded-xl p-2">
+            {/* <div className="bg-white p-5 rounded-xl flex h-80 justify-center items-center">
+              <Image
+                className="rounded-xl"
+                src={product.images[0]}
+                alt=""
+                width="240"
+                height="240"
+              />
+            </div> */}
+            <div className="h-auto"></div>
+            <div className=" h-40 relative">
+              <div className="mt-2">
+                <h3 className="font-bold text-lg">{product.title}</h3>
+              </div>
+              <p className="text-sm mt-1 leading-5">
+                {product.description.slice(0, 100)}...
+              </p>
+              <div className="h-auto"></div>
+              <div className="flex absolute pt-2 inset-x-0 bottom-0">
+                <div className="flex-grow text-2xl font-bold">
+                  ${product.price}
+                </div>
+                <button className="bg-black text-white py-1 px-3 rounded-xl">
+                  Buy Now
+                </button>
+              </div>
+            </div>
+          </div>
+        );
+      })}
     </Layout>
+  );
 }
