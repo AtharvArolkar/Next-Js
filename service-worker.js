db42504145a49afcbeafcfda803bfe4c72ae6ddd
floating-dynamo <shridharmaskeri@gmail.com>
2023-01-20 09:47:51 +0530

    Updated the next.config.js and service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,38 +1,5 @@
-import { skipWaiting, clientsClaim } from "workbox-core";
-import { ExpirationPlugin } from "workbox-expiration";
-import {
-  NetworkOnly,
-  NetworkFirst,
-  CacheFirst,
-  StaleWhileRevalidate,
-} from "workbox-strategies";
-import {
-  registerRoute,
-  setDefaultHandler,
-  setCatchHandler,
-} from "workbox-routing";
-import {
-  matchPrecache,
-  precacheAndRoute,
-  cleanupOutdatedCaches,
-} from "workbox-precaching";
-
 const staticCacheName = "site-static-v1";
 const assets = ["/"];
-
-skipWaiting();
-clientsClaim();
-
-// must include following lines when using inject manifest module from workbox
-// https://developers.google.com/web/tools/workbox/guides/precache-files/workbox-build#add_an_injection_point
-const WB_MANIFEST = self.__WB_MANIFEST;
-
-// Precache fallback route and image
-WB_MANIFEST.push({
-  url: "/fallback",
-  revision: "1234567890",
-});
-precacheAndRoute(WB_MANIFEST);
 
 // install event
 self.addEventListener("install", (evt) => {
